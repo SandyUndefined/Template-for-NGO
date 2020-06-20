d8dc139a131cfce82256c4a6ae0d0a7f39338d6b
Sandy_undefined <08sandysk@gmail.com>
2020-06-20 23:57:53 +0530

    Completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-/*setTimeout(function () {
-  $('.spinner').fadeToggle();
-},1500);*/
-
 var myvar;
 
 function myfunction() {
-  myvar = setTimeout(showPage,5000);
+  myvar = setTimeout(showPage,3000);
 }
 
 function showPage() {
   document.getElementById('loader').style.display = "none";
   document.getElementById('myDiv').style.display = "block";
 }
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 100) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
+$('.owl-carousel').owlCarousel({
+    loop:false,
+  stagePadding: 15,
+    margin:10,
+    nav:true,
+  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        640:{
+            items:2
+        },
+      960:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
